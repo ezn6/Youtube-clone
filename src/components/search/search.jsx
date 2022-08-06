95ca2fb7505d4aff534f5d910caf1d677e3b8c8c
ezn6 <kcourtney@naver.com>
2022-08-06 21:24:43 +0900

    상단 검색창 CSS

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from './search.module.css';
 
 const Search = (props) => {
   const [search, setSearch] = useState('');
@@ -7,18 +8,35 @@ const Search = (props) => {
     setSearch(e.target.value);
   };
 
+  const onSubmitSearch = (e) => {
+    e.preventDefault();
+    if (search === '') return;
+  };
+
   return (
-    <>
-      <i class='fa-brands fa-youtube'></i>
-      <span>Youtube</span>
-      <input
-        value={search}
-        onChange={onChangeSearch}
-        type='text'
-        placeholder='Search...'
-      />
-      <i class='fa-solid fa-magnifying-glass'></i>
-    </>
+    <div className={styles.background}>
+      <button className={styles.button}>
+        <img
+          className={styles.icon}
+          src='http://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/2560px-YouTube_full-color_icon_%282017%29.svg.png'
+          alt='youtube_logo'
+        />
+      </button>
+      <span className={styles.youtube}>Youtube</span>
+
+      <form className={styles.form} onSubmit={onSubmitSearch}>
+        <input
+          className={styles.searchInput}
+          value={search}
+          onChange={onChangeSearch}
+          type='text'
+          placeholder='Search...'
+        />
+        <button className={styles.button2}>
+          <i className='fa-solid fa-magnifying-glass'></i>
+        </button>
+      </form>
+    </div>
   );
 };
 
